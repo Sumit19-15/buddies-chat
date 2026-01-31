88bc6a9775de5f116be06c872f57f2620144833a
Sumit19-15 <2023kucp1028@iiitkota.ac.in>
2026-01-31 16:04:04 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,9 +4,11 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path";
 
 dotenv.config();
 const app = express();
+const __dirname = path.resolve(); // for production variable declare
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,10 +16,15 @@ const PORT = process.env.PORT || 3000;
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Listening on the Port ${PORT}`);
-});
-
-app.get("/", (req, res) => {
-  res.json("time is their");
 });
