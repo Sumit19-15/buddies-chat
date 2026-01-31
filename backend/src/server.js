96463d97aa58bbbdaf743a11d568cce732730910
Sumit19-15 <2023kucp1028@iiitkota.ac.in>
2026-01-31 15:11:58 +0530

    adding basic code regarding the router and all making basic folders as well

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,21 @@
 // main server file
 // const express = require("express"); in pack-json add type modules to use import
 import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+dotenv.config();
 const app = express();
 
-app.get("api/auth/signup", (req, res) => {
-  res.send("Signup endpoint");
-});
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+// declaring that go on Routes for this route
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Listening on the Port ${PORT}`);
 });
 
 app.get("/", (req, res) => {
