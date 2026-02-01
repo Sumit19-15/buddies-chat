053f29fb023695a5e20df4d6029851745b979862
Sumit19-15 <2023kucp1028@iiitkota.ac.in>
2026-02-01 14:41:02 +0530

    refactoring auth

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -74,6 +74,9 @@ export const signup = async (req, res) => {
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email ans Password is requuried" });
+  }
 
   try {
     const user = await User.findOne({ email });
