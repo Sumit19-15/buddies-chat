6d1b4461ff5750ec1e10b188ab456c4d103490bf
Sumit19-15 <2023kucp1028@iiitkota.ac.in>
2026-02-07 23:28:59 +0530

    adding the sigup page and toast-react for animated notification

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
 const BASE_URL =
   import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
@@ -7,18 +8,33 @@ const BASE_URL =
 // now function of the auth store
 
 export const useAuthStore = create((set) => ({
-  authUser: null,
-  isCheckingAuth: true,
-
+  authUser: null, // login user data store here
+  isCheckingAuth: true, // checking that authenticaiton checking is on or not
+  isSigningUp: false,
+  // checking auth function every time reload page
   checkAuth: async () => {
     try {
-      const res = await axiosInstance.get("auth/check");
+      const res = await axiosInstance.get("auth/check"); // backend requested here
       set({ authUser: res.data });
     } catch (error) {
       console.log("Error in authCheck", error);
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+
+      toast.success("Account created successfully!");
+    } catch (error) {
+      toast.error(error.response.data.message); // axios error messages handel in this way
+    } finally {
+      set({ isSigningUp: false });
     }
   },
 }));
