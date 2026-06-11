ecd95125aa4840e07421c82e7da3dbc8aa29378c
Sumit19-15 <2023kucp1028@iiitkota.ac.in>
2026-06-11 19:08:21 +0530

    deploy done

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,13 +26,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
+//   app.get("*", (_, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+//   });
+// }
 
 server.listen(PORT, () => {
   console.log(`Listening on the Port ${PORT}`);
